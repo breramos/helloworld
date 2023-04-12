6232cd58450f63613e66f6e91e24478e05c888fc
Brenda Ramos Bezerra <brendaramos.bezerra@gmail.com>
2023-04-12 14:17:16 -0300

    Update do Exercício de Spring

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -18,6 +18,27 @@ public class HelloController {
 		return "<b>Brenda está aqui!!!</b>";
 	}
 	
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "<b>BSM's Generation:<br>"
+				+ "<br>Mentalidade de Crescimento;<br>"
+				+ "<br>Persistência;<br>"
+				+ "<br>Responsabilidade Pessoal;<br>"
+				+ "<br>Orientação ao Futuro;<br>"
+				+ "<br>Comunicação;<br>"
+				+ "<br>Orientação ao Detalhe;<br>"
+				+ "<br>Proatividade;<br>"
+				+ "<br>Trabalho em Equipe<br></b>";
+	}
+	
+	@GetMapping("/objetivos")
+	public String objetivosAprendizagem() {
+		return "<b>Meus objetivos de aprendizagem da semana:<br>"
+				+ "<br>- Melhorar a orientação ao detalhe através de uma revisão cuidadosa de cada exercício feito;<br>"
+				+ "<br>- Aprender a usar o método pomodoro para aprimorar o foco na realização das tarefas;<br>"
+				+ "<br>- Praticar a meta SMART de estudar 1h todos os dias;<br></b>";
+	}
+	
 	
 	
 }
